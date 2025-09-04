ede764a43b2e8f8e1ad9e30c4fa985b67a89efd7
Korbin de Man <korbindeman4@gmail.com>
2025-09-04 19:38:36 +0200

    basic preview

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+from typing import T
 from picamera2 import Picamera2, Preview
 
 picam2 = Picamera2()
 picam2.start_preview(Preview.QTGL)
+picam2.start() # i don't know if this is necessary
+
+while True:
+    pass
